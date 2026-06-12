465ee06eac27114b107e6cc8d482be0e204d010b
aayushbhosale0606-droid <aayushbhosale0606@gmail.com>
2026-06-12 14:21:34 +0530

    Initial Commit

---
+++ db_config.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+def get_database_connection():
+    connection = mysql.connector.connect(
+        host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
+        user="4RBUcTRHeYRJodu.root",
+        password="2zAR75RiWhGUDdCg",
+        database="student_task_manager"
+        )
+    return connection
