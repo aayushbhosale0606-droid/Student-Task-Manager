66010f43be8d0bdb9476a78d9bf15056e3a736a9
shreyashmali1823 <shreyashmali1823@gmail.com>
2026-06-13 10:45:32 +0530

    Initial Commit

--- db_config.py
+++ db_config.py
@@ -3,8 +3,8 @@ import mysql.connector
 def get_database_connection():
     connection = mysql.connector.connect(
         host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
-        user="4RBUcTRHeYRJodu.root",
-        password="2zAR75RiWhGUDdCg",
-        database="student_task_manage"
+        user="2m4FnyFux21aQ6Z.root",
+        password="sQTHbGnSCJMyKY6c",
+        database="student_task_manager"
         )
     return connection
